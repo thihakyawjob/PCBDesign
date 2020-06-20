13300da3bb65d2978d188e6f87b737e531670817
thihakyaw <thihakyaw@thihakyawpc>
2020-06-20 12:21:16 +0800

    Update Relay code

--- ArduinoNano_TrainingBoard/CherryplazaBoard/ArduinoCodes/Relay/Relay.ino
+++ ArduinoNano_TrainingBoard/CherryplazaBoard/ArduinoCodes/Relay/Relay.ino
@@ -1,6 +1,7 @@
 /*
-  Blink
+  Relay
 
+  Turns an Relay on for one second, then off for one second.
   Turns an LED1 and LED2 on for one second, then off for one second, repeatedly.
 
   modified 20 Jun 2020
@@ -8,21 +9,30 @@
 
   This example code is for Cherryplaza Arduino Training Board
 */
+int Relay=A3;
+
 int LED1=6;
 int LED2=7;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
+  // initialize Analog pin Relay as an output.
+  pinMode(Relay, OUTPUT);
+
   // initialize digital pins, LED1 and LED2 as an output.
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  digitalWrite(Relay, HIGH);   // turn the Relay on (HIGH is the voltage level)
   digitalWrite(LED1, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED2, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
+  delay(1000);                       // wait for a second
+  digitalWrite(Relay, LOW);    // turn the Relay off by making the voltage LOW
   digitalWrite(LED1, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(LED2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+  delay(1000);                       // wait for a second
 }
